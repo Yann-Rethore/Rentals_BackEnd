500f1c1d86420a9f516c15c348c0854e1dbabaf5
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-04-21 10:31:40 +0200

    Ajout des classes de l'authentification après révision
    AuthController.java
    UserDTO.java
    UserService.java

--- Rental/Rental/src/main/java/com/openclassroom/Rental/Controller/AuthController.java
+++ Rental/Rental/src/main/java/com/openclassroom/Rental/Controller/AuthController.java
@@ -1,21 +1,28 @@
 package com.openclassroom.Rental.Controller;
 
 import com.openclassroom.Rental.Configuration.JwtUtil;
-import com.openclassroom.Rental.DTO.LoginRequest;
-import com.openclassroom.Rental.Entities.Users;
+import com.openclassroom.Rental.DTO.UserDTO;
 import com.openclassroom.Rental.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Authentication")
 public class AuthController {
 
-    //private final
+    @Autowired
     private final UserService userService;
+
+    @Autowired
     private final JwtUtil jwtUtil;
 
     public AuthController(UserService userService, JwtUtil jwtUtil) {
@@ -23,61 +30,92 @@ public class AuthController {
         this.jwtUtil = jwtUtil;
     }
 
-
+    @Operation(summary = "Enregistrement d'un utilisateur")
     @PostMapping("/register")
-    public ResponseEntity<?> registerAndReturnUser(@RequestBody Users user) {
+    public ResponseEntity<?> registerAndReturnUser(@RequestBody UserDTO user) {
 
             if (user.getName() == null || user.getEmail() == null || user.getPassword() == null) {        // Validation des champs
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
         }
 
-
-
         // Enregistrement de l'utilisateur
-
             userService.registerUser(user);
-            String token = jwtUtil.generateToken(user.getName());
 
-        return ResponseEntity.ok("{\"token\": \"" + token + "\"}");
+            // Création du token
+            String token = jwtUtil.generateToken(user.getEmail());
+
+            return ResponseEntity.ok("{\"token\": \"" + token + "\"}");
+
+
     }
 
+    @Operation(summary = "Connexion d'un utilisateur")
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    //public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        public ResponseEntity<?> login(@RequestBody UserDTO loginRequest) {
+        // Invalider le token en cours
+        SecurityContextHolder.clearContext();
+        System.out.println("loginRequest = " + loginRequest.getEmail() + "  " + loginRequest.getPassword());
 
         // Validation des champs
         if (loginRequest.getEmail() == null || loginRequest.getPassword() == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\": \"Email et mot de passe requis\"}");
         }
 
+
         String email = loginRequest.getEmail();
         String password = loginRequest.getPassword();
         // Vérification des valeurs
-        Users user = userService.findByEmailAndValidatePassword(email, password);
+        UserDTO user = userService.findByEmailAndValidatePassword(email, password);
 
-            // Si les valeurs ne correspondent pas, renvoyer une erreur 404
-            if (user == null) {return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\": \"Identifiants invalides\"}");}
+            // Si les valeurs ne correspondent pas, renvoyer une erreur 401
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"error\": \"Identifiants invalides\"}");
+            }
+            else {
+                System.out.println("user = " + user.getId());
             // Génération du token JWT
-            String token = jwtUtil.generateToken(user.getName());
+            String token = jwtUtil.generateToken(email);
 
             // Réponse avec le token
-        return ResponseEntity.ok("{\"token\": \"" + token + "\"}");
+                return ResponseEntity.ok("{\"token\": \"" + token + "\"}");
 
         }
-
-        @GetMapping("/me")
-        public ResponseEntity<?> getCurrentUser() {
-            var authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication == null || !authentication.isAuthenticated()) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"error\": \"Utilisateur non authentifié\"}");
-            }
-            return ResponseEntity.status(HttpStatus.OK)
-                    .build();
-        }
-
-
-
 
     }
+
+    @Operation(summary = "Récupérer les informations de l'utilisateur connecté")
+    @GetMapping("me")
+    public ResponseEntity<?> getCurrentUser(@RequestHeader(value = "Authorization", required = false) String authorizationHeader) {
+        // Vérifiez si l'en-tête Authorization est présent
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{}");
+        }
+        // Extraire le jeton
+        String token = authorizationHeader.substring(7);
+
+        // Récupérer les informations de l'utilisateur à partir du jeton
+        String email = jwtUtil.extractUsername(token);
+
+        UserDTO user = userService.findByEmailAndGetUser(email);
+
+        String id = user.getId().toString();
+        String name = user.getName();
+        Timestamp createdAt = user.getCreatedAt();
+        Timestamp updatedAt = user.getUpdatedAt();
+
+
+        // réponse utilisateur
+        return ResponseEntity.ok("{\n" +
+                "  \"id\": \"" + id + "\",\n" +
+                "  \"name\": \"" + name + "\",\n" +
+                "  \"email\": \"" + email + "\",\n" +
+                "  \"created_at\": \"" + createdAt.toLocalDateTime() + "\",\n" +
+                "  \"updated_at\": \"" + updatedAt.toLocalDateTime() + "\"\n" +
+                "}");
+    }
+}
+
 
 
 
