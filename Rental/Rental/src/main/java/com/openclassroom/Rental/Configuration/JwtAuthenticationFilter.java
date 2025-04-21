9c04e6797cf61c3f8230b1f235995e56462cf6f9
Yann-Rethore <yann.rethore.pin@gmail.com>
2025-04-21 10:41:21 +0200

    Ajout des classes de l'authentification après révision
    AuthController.java
    UserDTO.java
    UserService.java

--- Rental/Rental/src/main/java/com/openclassroom/Rental/Configuration/JwtAuthenticationFilter.java
+++ Rental/Rental/src/main/java/com/openclassroom/Rental/Configuration/JwtAuthenticationFilter.java
@@ -1,13 +1,15 @@
 package com.openclassroom.Rental.Configuration;
 
+import com.openclassroom.Rental.DTO.UserDTO;
+import com.openclassroom.Rental.Repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,40 +19,52 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
     private final JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+    @Autowired
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
-        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain chain)
             throws ServletException, IOException {
-        String path = request.getServletPath();
-        if (path.startsWith("/api/auth/")) {
-            chain.doFilter(request, response); // Ignore le filtre pour /auth/*
-            return; // Ajouté pour éviter de continuer le traitement
-        }
-
-
 
 
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
-            String username = jwtUtil.extractUsername(token);
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            String email = jwtUtil.extractUsername(token);
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDTO user = userRepository.findByEmail(email);
                 if (jwtUtil.validateToken(token)) {
                     var authToken = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities());
+                            user, null, null);
                     authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
         }
+
         chain.doFilter(request, response);
     }
 }
+  /* @Override
+   protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request,
+                                   jakarta.servlet.http.@NotNull HttpServletResponse response, jakarta.servlet.FilterChain filterChain)
+           throws jakarta.servlet.ServletException, IOException {
+
+       final String requestTokenHeader = request.getHeader("Authorization");
+
+       // JWT Token is in the form "Bearer token". Remove Bearer word and get only the
+       // Token
+       if (requestTokenHeader == null && !requestTokenHeader.startsWith("Bearer ")) {
+           logger.warn("JWT Token does not begin with Bearer String");
+       }
+       filterChain.doFilter(request, response);
+   }*/
+
